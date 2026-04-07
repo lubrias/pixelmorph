12ef3bad3bba720e284725fb0586e99ac9e0b72e
W. Luan <109882978+lubrias@users.noreply.github.com>
2026-04-07 18:35:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const themeToggle = document.getElementById('themeToggle');
-const API_URL = 'https://pixelmorph.onrender.com';
+const API_URL = 'https://pixelmorph-xyz.onrender.com';
 const root = document.documentElement;
 const dropZone = document.getElementById('dropZone');
 const fileInput = document.getElementById('fileInput');
